3a9cc259581a25bebdca2e18f75ec3e3d09c0e35
Abraham <abraham81423@gmail.com>
2022-06-12 02:23:30 -0600

    [UPDATE] Arduino

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,42 @@
+/* EEPROM */
 #include <EEPROM.h>
+/* Pantalla Led */
+#include<LedControl.h>
+#include <LiquidCrystal_I2C.h>
+/* Teclado Matricial */
+#include <Keypad.h>
 
 /* bluetooth */
 #define bluethoot 8
 /* sensor de temperatura */
 #define pinA1 1
 
+/* TIME */
+unsigned long time_matrix = 0;
+unsigned long contador_1  = 0; 
+
 /* Variables */
 float temperature_C;
+/************************/
+int contador = 0 ; 
+boolean mensaje = false ; 
+
+
+/* Create Matrix Keyboard */
+
+const byte matrix_row     = 4;
+const byte matrix_column  = 3;
+ 
+char keys[matrix_row][matrix_column] = {
+   { '1','2','3'},
+   { '4','5','6'},
+   { '7','8','9'},
+   { '*','0','#'}
+};
+
+/* PINES DE TECLADO MATRICIAL */
+const byte matrix_row_pin[matrix_row]       = { 30, 31, 32, 33};
+const byte matrix_column_pin[matrix_column] = { 34, 35, 36};
 
 /* Create Struct */
 int eeAddress = 0;
@@ -20,8 +50,12 @@ struct MyStruct{
   float temperature;
 };
 
-/* Assignment */
+/* Assignment Struct */
 MyStruct MyStructValue = {10,10,10,10,10,0.0,0.0};
+
+/* Declaration */
+Keypad keypad = Keypad(makeKeymap(keys), matrix_row_pin, matrix_column_pin, matrix_row, matrix_column);
+LiquidCrystal_I2C lcd(0x27,20,4);
 
 void setup() {
   // put your setup code here, to run once:
@@ -32,6 +66,9 @@ void setup() {
   pinMode(bluethoot, OUTPUT);
   /* EEPROM */
   EEPROM.put(eeAddress, MyStructValue);
+  /* PANTALLA LCD */
+  lcd.begin(16,2);
+  lcd.clear();
 }
 
 void loop() {
@@ -95,4 +132,42 @@ void loop() {
 
   }
 
+}
+
+/* PANTALLA LCD */
+void metodoTeclado(){
+   if (millis() > time_matrix + 10){
+    time_matrix = millis();
+
+    char key = keypad.getKey();
+    if (key) {
+      Serial.println(key);
+    }
+  }
+}
+
+
+/* MATRIZ KEYBOARD */
+void metodoMensaje(){
+    
+  if(millis()<contador_1+1500){
+    if(mensaje != true){
+      contador_1 = millis() + 1500;  
+    }
+       
+    lcd.setCursor(0,0);
+    lcd.print("Grupo 3");
+    lcd.setCursor(2,1);
+    lcd.print("Proyecto 1");
+    mensaje = true;
+  }else{
+    if(millis()>contador_1){
+      if(mensaje == true){
+        lcd.clear();
+        mensaje = false ; 
+      }
+      lcd.setCursor(2,1);
+      metodoTeclado();
+    }
+  }
 }
