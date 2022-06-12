9caf7ebee7bcc3d8d093b98456490b28660e8789
Abraham <abraham81423@gmail.com>
2022-06-12 01:24:12 -0600

    [ADD] Teclado Matricial

--- MasterSlave/codigoMaestro/codigoMaestro.ino
+++ MasterSlave/codigoMaestro/codigoMaestro.ino
@@ -1,24 +1,56 @@
-#include <Wire.h> 
-#include <MatrizLed.h>
-#include <Key.h>
-#include <Keypad.h>
-#include <LiquidCrystal_I2C.h>
-#include <Stepper.h>
-#include <Servo.h>
+/* Pantalla Led */
 #include<LedControl.h>
+#include <LiquidCrystal_I2C.h>
 
+/* Teclado Matricial */
+#include <Keypad.h>
 
-LiquidCrystal_I2C lcd(0x27,20,4);
+/* TIME */
+unsigned long time_matrix = 0;
+
+/* Variables */
 int contador = 0 ; 
 boolean mensaje = false ; 
+
+const byte matrix_row     = 4;
+const byte matrix_column  = 3;
+ 
+char keys[matrix_row][matrix_column] = {
+   { '1','2','3'},
+   { '4','5','6'},
+   { '7','8','9'},
+   { '*','0','#'}
+};
+
+const byte matrix_row_pin[matrix_row]       = { 30, 31, 32, 33};
+const byte matrix_column_pin[matrix_column] = { 34, 35, 36};
+
+
+/* Definition Object */
+LiquidCrystal_I2C lcd(0x27,20,4);
+
+Keypad keypad = Keypad(makeKeymap(keys), matrix_row_pin, matrix_column_pin, matrix_row, matrix_column);
+
 void setup() {
+  Serial.begin(9600);
   lcd.begin(16,2);
   lcd.print("Mancos qliados");
 
 }
 
 void loop() {
- metodoMensaje();
+  
+ //Serial.println("hola");
+ //metodoMensaje();
+  if (millis() > time_matrix + 10){
+    time_matrix = millis();
+
+    char key = keypad.getKey();
+    if (key) {
+      Serial.println(key);
+    }
+  }
+ 
 }
 
 void metodoMensaje(){
